f03f6e54689743817161b57ed9d0e38937aaaecf
ashkan <bagheriashkan77@gmail.com>
2022-05-15 03:05:53 +0430

    Add files via upload

--- model.py
+++ model.py
@@ -46,7 +46,7 @@ class Model(nn.Module):
     x = self.d_conv1(x)
     x = self.d_conv2(x)
     x = self.d_conv3(x)
-    x = flatten(x)
+    x = flatten(x, 1)
     x = self.fc1(x)
     x = relu(x)
     x = self.fc2(x)
